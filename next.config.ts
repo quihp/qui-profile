cfb8676f9eaf3b6cb170328ba5443d88ae912f9f
quihp <quiquihuynh@gmail.com>
2024-11-10 10:59:45 +0700

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    dangerouslyAllowSVG: true,
+    remotePatterns: [{ hostname: "cdn.sanity.io" }]
+  },
+  eslint: { ignoreDuringBuilds: true, }
 };
 
 export default nextConfig;
